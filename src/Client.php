ff88ca1b8a42914a42b9baa3fc3226119c8d71fd
Sundari <sundari@gegosoft.com>
2020-07-28 22:35:17 +0530

    remove wallet issue

--- src/Client.php
+++ src/Client.php
@@ -109,16 +109,7 @@ class Client
     {
         try {
            $uri='/';
-             //ADD
-          if(count($params)>0)
-           {
-                if(isset($params[0]['wallet']['is_url_wallet']) && $params[0]['wallet']['is_url_wallet']==true)
-                {
-                    $uri=$uri.'wallet/'.$params[0]['wallet']['wallet_name'];
-                    unset($params[0]);
-                    $params = array_values($params);
-                }
-           }
+          
         
             $json = [
                 'method' => strtolower($method),
